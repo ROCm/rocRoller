f669ae783eb2d780d66749960b19d1f8b6f85519
João P. L. Carvalho <3893451+jaopaulolc@users.noreply.github.com>
2025-11-14 16:15:39 +0000

    [rocm-libraries] ROCm/rocm-libraries#2643 (commit 9d2fb9a)
    [rocroller] Only use pullRequest property on PRs
    
    ## Motivation
    
    Fix issue introduced by #2410 where the property `pullRequest` was
    accessed on both PR and non-PR paths of the groovy code executed as part
    of rocRoller's performance job.
    
    ## Technical Details
    
    See motivation.
    
    ## Test Plan
    
    See the performance job working after this PR is merged.
    
    ## Test Result
    
    Performance job runs and produces artifact.
    
    ## Submission Checklist
    
    - [X] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    Co-authored-by: Chun-Xun Lin <Chun-Xun.Lin@amd.com>

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -224,17 +224,17 @@ def runPerformanceCommand (platform, project, mxDataGeneratorGitURL, mxDataGener
         sshBlock ->
         def rrperfSuite = platform.jenkinsLabel.contains('gfx12') ? "all_gfx120X" : "all"
 
-        // either a label or a parameter can block comparison to master branch
-        def masterCompare = !(
-            pullRequest.labels.any { it == "ci:no-build-master" || it == "ci:no-build-target" }
-        )
-        if (masterCompare && (params?."Build target branch for comparison" != null))
-        {
-            masterCompare = params."Build target branch for comparison"
-        }
-
         if (env.CHANGE_ID)
         {
+            // either a label or a parameter can block comparison to master branch
+            def masterCompare = !(
+                pullRequest.labels.any { it == "ci:no-build-master" || it == "ci:no-build-target" }
+            )
+            if (masterCompare && (params?."Build target branch for comparison" != null))
+            {
+                masterCompare = params."Build target branch for comparison"
+            }
+
             String masterCompareCommand
             if (masterCompare)
             {
@@ -364,6 +364,13 @@ def runPerformanceCommand (platform, project, mxDataGeneratorGitURL, mxDataGener
         else
         {
             def ARCHIVE_LIMIT = "101"
+
+            // a parameter can block comparison to target branch
+            def masterCompare = false
+            if (params?."Build target branch for comparison" != null)
+            {
+                masterCompare = params."Build target branch for comparison"
+            }
 
             String masterCompareString = masterCompare ? "1" : "0"
 
